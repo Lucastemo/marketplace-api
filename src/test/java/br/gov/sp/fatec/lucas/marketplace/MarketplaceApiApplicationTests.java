a66e506134288560beb2b331bd26a71726c80710
LucasTM <lucasteixeiramoraes15@gmail.com>
2026-01-26 18:43:30 -0300

    chore: initial project setup from spring initializr

---
+++ src/test/java/br/gov/sp/fatec/lucas/marketplace/MarketplaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.lucas.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

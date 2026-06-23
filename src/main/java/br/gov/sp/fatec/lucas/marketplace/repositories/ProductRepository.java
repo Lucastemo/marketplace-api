fde32b8456f0797eddea82ad14f8b22d4badb042
LucasTM <lucasteixeiramoraes15@gmail.com>
2026-06-23 18:37:50 -0300

    feat(product): create repository

---
+++ src/main/java/br/gov/sp/fatec/lucas/marketplace/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package br.gov.sp.fatec.lucas.marketplace.repositories;
+
+import br.gov.sp.fatec.lucas.marketplace.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}

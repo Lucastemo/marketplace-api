70dbe94f37cf7bd7ff4cd7e50097a110c7712e81
LucasTM <lucasteixeiramoraes15@gmail.com>
2026-06-25 11:15:14 -0300

    feat(product): create service with createProduct and mapper methods

---
+++ src/main/java/br/gov/sp/fatec/lucas/marketplace/services/ProductService.java
@@ -0,0 +1,46 @@
+package br.gov.sp.fatec.lucas.marketplace.services;
+
+import br.gov.sp.fatec.lucas.marketplace.dtos.CategoryResponseDTO;
+import br.gov.sp.fatec.lucas.marketplace.dtos.ProductRequestDTO;
+import br.gov.sp.fatec.lucas.marketplace.dtos.ProductResponseDTO;
+import br.gov.sp.fatec.lucas.marketplace.entities.Category;
+import br.gov.sp.fatec.lucas.marketplace.entities.Product;
+import br.gov.sp.fatec.lucas.marketplace.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final CategoryService categoryService;
+
+    public ProductResponseDTO createProduct(ProductRequestDTO requestData){
+        Product product = dtoToEntity(requestData);
+        return entityToDto(productRepository.save(product));
+    }
+
+    public ProductResponseDTO entityToDto(Product productEntity){
+        CategoryResponseDTO categoryResponseDTO = categoryService.entityToDto(productEntity.getCategory());
+        return new ProductResponseDTO(
+                productEntity.getId(),
+                productEntity.getName(),
+                productEntity.getDescription(),
+                productEntity.getPrice(),
+                productEntity.getImgUrl(),
+                categoryResponseDTO
+        );
+    }
+
+    public Product dtoToEntity(ProductRequestDTO productRequestDTO){
+        Category category = categoryService.findCategoryById(productRequestDTO.categoryId());
+        Product productEntity = new Product();
+        productEntity.setName(productRequestDTO.name());
+        productEntity.setDescription(productRequestDTO.description());
+        productEntity.setPrice(productRequestDTO.price());
+        productEntity.setImgUrl(productRequestDTO.imgUrl());
+        productEntity.setCategory(category);
+
+        return productEntity;
+    }
+}

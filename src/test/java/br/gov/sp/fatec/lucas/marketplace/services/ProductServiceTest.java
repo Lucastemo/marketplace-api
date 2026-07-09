d4b6c302270ac3af727ff496fbf78f3892c4d50a
LucasTM <lucasteixeiramoraes15@gmail.com>
2026-07-09 13:17:25 -0300

    refactor(test): extract test data generation into methods for ProductServiceTest

--- src/test/java/br/gov/sp/fatec/lucas/marketplace/services/ProductServiceTest.java
+++ src/test/java/br/gov/sp/fatec/lucas/marketplace/services/ProductServiceTest.java
@@ -35,171 +35,87 @@ class ProductServiceTest {
 
     @Test
     void createProduct_ShouldReturnProductResponseDTO_WhenSuccessful(){
-        Category foundCategory = new Category(
-                2L,
-                "Electronics"
-        );
+        Category foundCategory = createFakeCategory();
 
-        CategoryResponseDTO categoryDTO = new CategoryResponseDTO(
-                2L,
-                "Electronics"
-        );
+        CategoryResponseDTO categoryDTO = entityToResponseDTO(foundCategory);
 
-        Product createdProduct = new Product(
-                1L,
-                "Keyboard",
-                "Mechanical Keyboard with RGB Lightning",
-                BigDecimal.valueOf(250.00),
-                "https://my-api-images.com/keyboard.jpg",
-                foundCategory
-        );
+        Product createdProduct = createFakeProduct(foundCategory);
 
-        ProductRequestDTO testRequestDTO = new ProductRequestDTO(
-                "Keyboard",
-                "Mechanical Keyboard with RGB Lightning",
-                BigDecimal.valueOf(250.00),
-                "https://my-api-images.com/keyboard.jpg",
-                2L
-        );
+        ProductRequestDTO testRequestDTO = createFakeProductRequestDTO(foundCategory.getId());
 
-        Mockito.when(categoryService.findCategoryById(2L)).thenReturn(foundCategory);
+        Mockito.when(categoryService.findCategoryById(foundCategory.getId())).thenReturn(foundCategory);
         Mockito.when(categoryService.entityToDto(any(Category.class))).thenReturn(categoryDTO);
         Mockito.when(productRepository.save(any(Product.class))).thenReturn(createdProduct);
 
         ProductResponseDTO response = productService.createProduct(testRequestDTO);
 
         assertNotNull(response);
-        assertEquals(1L, response.id());
-        assertEquals("Keyboard", response.name());
-        assertEquals("Mechanical Keyboard with RGB Lightning", response.description());
-        assertEquals(BigDecimal.valueOf(250.00), response.price());
-        assertEquals("https://my-api-images.com/keyboard.jpg", response.imgUrl());
-        assertEquals(2L, response.category().id());
-        assertEquals("Electronics", response.category().name());
+
+        ProductResponseDTO expectedResponse = createExpectedResponse(createdProduct);
+
+        assertEquals(expectedResponse, response);
     }
 
     @Test
     void findProductsByCategoryId_ShouldReturnProductResponseDTOList_WhenSuccessful(){
-        Category category = new Category(
-                2L,
-                "Electronics"
-        );
+        Category category = createFakeCategory();
 
-        CategoryResponseDTO categoryDTO = new CategoryResponseDTO(
-                2L,
-                "Electronics"
-        );
+        CategoryResponseDTO categoryDTO = entityToResponseDTO(category);
 
-        Product product_1 = new Product(
-                1L,
-                "Keyboard",
-                "Mechanical Keyboard with RGB Lightning",
-                BigDecimal.valueOf(250.00),
-                "https://my-api-images.com/keyboard.jpg",
-                category
-        );
+        Product product_1 = createFakeProduct(category, 0);
 
-        Product product_2 = new Product(
-                2L,
-                "Mouse",
-                "Mouse with RGB Lightning",
-                BigDecimal.valueOf(60.00),
-                "https://my-api-images.com/mouse.jpg",
-                category
-        );
+        Product product_2 = createFakeProduct(category, 1);
 
         List<Product> foundProductsList = List.of(product_1, product_2);
 
-        Mockito.when(productRepository.findByCategoryId(2L)).thenReturn(foundProductsList);
+        Mockito.when(productRepository.findByCategoryId(category.getId())).thenReturn(foundProductsList);
         Mockito.when(categoryService.entityToDto(any(Category.class))).thenReturn(categoryDTO);
 
-        List<ProductResponseDTO> response = productService.findProductsByCategoryId(2L);
+        List<ProductResponseDTO> response = productService.findProductsByCategoryId(category.getId());
 
         assertNotNull(response);
 
-        assertEquals(1L, response.get(0).id());
-        assertEquals("Keyboard", response.get(0).name());
-        assertEquals("Mechanical Keyboard with RGB Lightning", response.get(0).description());
-        assertEquals(BigDecimal.valueOf(250.00), response.get(0).price());
-        assertEquals("https://my-api-images.com/keyboard.jpg", response.get(0).imgUrl());
-        assertEquals(2L, response.get(0).category().id());
+        ProductResponseDTO expectedResponse1 = createExpectedResponse(product_1);
 
-        assertEquals(2L, response.get(1).id());
-        assertEquals("Mouse", response.get(1).name());
-        assertEquals("Mouse with RGB Lightning", response.get(1).description());
-        assertEquals(BigDecimal.valueOf(60.00), response.get(1).price());
-        assertEquals("https://my-api-images.com/mouse.jpg", response.get(1).imgUrl());
-        assertEquals(2L, response.get(1).category().id());
-        assertEquals("Electronics", response.get(1).category().name());
+        assertEquals(expectedResponse1, response.get(0));
+
+        ProductResponseDTO expectedResponse2 = createExpectedResponse(product_2);
+
+        assertEquals(expectedResponse2, response.get(1));
     }
 
     @Test
     void findProductById_ShouldReturnProductResponseDTO_WhenSuccessful(){
-        Category category = new Category(
-                2L,
-                "Electronics"
-        );
+        Category category = createFakeCategory();
 
-        CategoryResponseDTO categoryDTO = new CategoryResponseDTO(
-                2L,
-                "Electronics"
-        );
+        CategoryResponseDTO categoryDTO = entityToResponseDTO(category);
 
-        Product foundProduct = new Product(
-                1L,
-                "Keyboard",
-                "Mechanical Keyboard with RGB Lightning",
-                BigDecimal.valueOf(250.00),
-                "https://my-api-images.com/keyboard.jpg",
-                category
-        );
+        Product foundProduct = createFakeProduct(category);
 
-        Mockito.when(productRepository.findById(1L)).thenReturn(Optional.of(foundProduct));
+        Mockito.when(productRepository.findById(foundProduct.getId())).thenReturn(Optional.of(foundProduct));
         Mockito.when(categoryService.entityToDto(any(Category.class))).thenReturn(categoryDTO);
 
-        ProductResponseDTO response = productService.findProductById(1L);
+        ProductResponseDTO response = productService.findProductById(foundProduct.getId());
+
+        ProductResponseDTO expectedResponse = createExpectedResponse(foundProduct);
 
         assertNotNull(response);
-        assertEquals(1L, response.id());
-        assertEquals("Keyboard", response.name());
-        assertEquals("Mechanical Keyboard with RGB Lightning", response.description());
-        assertEquals(BigDecimal.valueOf(250.00), response.price());
-        assertEquals("https://my-api-images.com/keyboard.jpg", response.imgUrl());
-        assertEquals(2L, response.category().id());
-        assertEquals("Electronics", response.category().name());
+
+        assertEquals(expectedResponse, response);
     }
 
     @Test
     void updateProduct_ShouldReturnProductResponseDTO_WhenSuccessful(){
-        Category foundCategory = new Category(
-                2L,
-                "Electronics"
-        );
+        Category foundCategory = createFakeCategory();
 
-        CategoryResponseDTO categoryDTO = new CategoryResponseDTO(
-                2L,
-                "Electronics"
-        );
+        CategoryResponseDTO categoryDTO = entityToResponseDTO(foundCategory);
 
-        Product existingProduct = new Product(
-                1L,
-                "Keyboard",
-                "Mechanical Keyboard with RGB Lightning",
-                BigDecimal.valueOf(250.00),
-                "https://my-api-images.com/keyboard.jpg",
-                foundCategory
-        );
+        Product existingProduct = createFakeProduct(foundCategory);
 
-        ProductRequestDTO updatedProduct = new ProductRequestDTO(
-                "Mechanical Keyboard",
-                "A Great Keyboard with RGB Lightning",
-                BigDecimal.valueOf(275.00),
-                "https://my-api-images.com/mechanical-keyboard.jpg",
-                2L
-        );
+        ProductRequestDTO updatedProduct = entityToRequestDTO(updateFakeProduct(existingProduct, foundCategory));
 
-        Mockito.when(categoryService.findCategoryById(2L)).thenReturn(foundCategory);
-        Mockito.when(productRepository.findById(1L)).thenReturn(Optional.of(existingProduct));
+        Mockito.when(categoryService.findCategoryById(foundCategory.getId())).thenReturn(foundCategory);
+        Mockito.when(productRepository.findById(existingProduct.getId())).thenReturn(Optional.of(existingProduct));
         Mockito.when(productRepository.save(any(Product.class))).thenAnswer(invocation -> {
             Product inputProduct = invocation.getArgument(0);
 
@@ -214,80 +130,193 @@ class ProductServiceTest {
         });
         Mockito.when(categoryService.entityToDto(any(Category.class))).thenReturn(categoryDTO);
 
-        ProductResponseDTO response = productService.updateProduct(1L, updatedProduct);
+        ProductResponseDTO response = productService.updateProduct(existingProduct.getId(), updatedProduct);
 
         assertNotNull(response);
 
-        assertEquals(1L, response.id());
-        assertEquals("Mechanical Keyboard", response.name());
-        assertEquals("A Great Keyboard with RGB Lightning", response.description());
-        assertEquals(BigDecimal.valueOf(275.00), response.price());
-        assertEquals("https://my-api-images.com/mechanical-keyboard.jpg", response.imgUrl());
-        assertEquals(2L, response.category().id());
-        assertEquals("Electronics", response.category().name());
+        ProductResponseDTO expectedResponse = createExpectedResponse(updateFakeProduct(existingProduct, foundCategory));
+
+        assertEquals(expectedResponse, response);
     }
 
     @Test
     void deleteProduct_ShouldExecuteWithoutErrors_WhenSuccessful(){
-        Product foundProduct = new Product(
-                1L,
-                "Keyboard",
-                "Mechanical Keyboard with RGB Lightning",
-                BigDecimal.valueOf(250.00),
-                "https://my-api-images.com/keyboard.jpg",
-                new Category(
-                        2L,
-                        "Electronics"
-                )
-        );
+        Product foundProduct = createFakeProduct(createFakeCategory());
 
-        Mockito.when(productRepository.findById(1L)).thenReturn(Optional.of(foundProduct));
+        Mockito.when(productRepository.findById(foundProduct.getId())).thenReturn(Optional.of(foundProduct));
 
-        assertDoesNotThrow(() -> productService.deleteProduct(1L));
+        assertDoesNotThrow(() -> productService.deleteProduct(foundProduct.getId()));
 
         Mockito.verify(productRepository, Mockito.times(1)).delete(foundProduct);
     }
 
     @Test
     void findProductById_ShouldReturnResourceNotFoundException_WhenIDDoesNotExist(){
-        Mockito.when(productRepository.findById(999L)).thenReturn(Optional.empty());
+        Long nonexistentId = 999L;
+
+        Mockito.when(productRepository.findById(nonexistentId)).thenReturn(Optional.empty());
 
         Exception exception = assertThrows(ResourceNotFoundException.class, () -> {
-            productService.findProductById(999L);
+            productService.findProductById(nonexistentId);
         });
 
-        assertEquals("Product not found with ID: 999", exception.getMessage());
+        String expectedMessage = getExpectedProductNotFoundMessage(nonexistentId);
+
+        assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
     void updateProduct_ShouldReturnResourceNotFoundException_WhenIDDoesNotExist(){
-        ProductRequestDTO updatedProduct = new ProductRequestDTO(
-                "Mechanical Keyboard",
-                "A Great Keyboard with RGB Lightning",
-                BigDecimal.valueOf(275.00),
-                "https://my-api-images.com/mechanical-keyboard.jpg",
-                2L
-        );
+        Long nonexistentId = 999L;
 
-        Mockito.when(productRepository.findById(999L)).thenReturn(Optional.empty());
+        Category category = createFakeCategory();
+        ProductRequestDTO updatedProduct = entityToRequestDTO(updateFakeProduct(createFakeProduct(category), category));
+
+        Mockito.when(productRepository.findById(nonexistentId)).thenReturn(Optional.empty());
 
         Exception exception = assertThrows(ResourceNotFoundException.class, () -> {
-            productService.updateProduct(999L, updatedProduct);
+            productService.updateProduct(nonexistentId, updatedProduct);
         });
 
-        assertEquals("Product not found with ID: 999", exception.getMessage());
+        String expectedMessage = getExpectedProductNotFoundMessage(nonexistentId);
+
+        assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
     void deleteProduct_ShouldReturnResourceNotFoundException_WhenIDDoesNotExist(){
-        Mockito.when(productRepository.findById(999L)).thenReturn(Optional.empty());
+        Long nonexistentId = 999L;
+
+        Mockito.when(productRepository.findById(nonexistentId)).thenReturn(Optional.empty());
 
         Exception exception = assertThrows(ResourceNotFoundException.class, () -> {
-            productService.deleteProduct(999L);
+            productService.deleteProduct(nonexistentId);
         });
 
         Mockito.verify(productRepository, Mockito.times(0)).delete(any(Product.class));
 
-        assertEquals("Product not found with ID: 999", exception.getMessage());
+        String expectedMessage = getExpectedProductNotFoundMessage(nonexistentId);
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    Category createFakeCategory(){
+        return new Category(
+                2L,
+                "Electronics"
+        );
+    }
+
+    Product createFakeProduct(Category category, Integer option){
+        switch(option){
+            case 0:
+                return new Product(
+                        1L,
+                        "Keyboard",
+                        "Mechanical Keyboard with RGB Lightning",
+                        BigDecimal.valueOf(250.00),
+                        "https://my-api-images.com/keyboard.jpg",
+                        category
+                );
+            case 1:
+                return new Product(
+                        2L,
+                        "Mouse",
+                        "Mouse with RGB Lightning",
+                        BigDecimal.valueOf(60.00),
+                        "https://my-api-images.com/mouse.jpg",
+                        category
+                );
+            default:
+                return new Product(
+                        0L,
+                        "Nonexistent Product",
+                        "Invalid option on createFakeProduct method",
+                        BigDecimal.valueOf(0.00),
+                        "no-product",
+                        new Category(
+                                0L,
+                                "Nonexistent Category"
+                        )
+                );
+        }
+    }
+
+    Product createFakeProduct(Category category){
+        return new Product(
+                1L,
+                "Keyboard",
+                "Mechanical Keyboard with RGB Lightning",
+                BigDecimal.valueOf(250.00),
+                "https://my-api-images.com/keyboard.jpg",
+                category
+        );
+    }
+
+    Product updateFakeProduct(Product product, Category category){
+        if (product.getId() == 1L) {
+            return new Product(
+                    product.getId(),
+                    "Mechanical Keyboard",
+                    "A Great Keyboard with RGB Lightning",
+                    BigDecimal.valueOf(275.00),
+                    "https://my-api-images.com/mechanical-keyboard.jpg",
+                    category
+            );
+        }else{
+            return new Product(
+                    0L,
+                    "Nonexistent Product",
+                    "This product does not exist in the createFakeProduct method",
+                    BigDecimal.valueOf(0.00),
+                    "no-product",
+                    new Category(
+                            0L,
+                            "Nonexistent Category"
+                    )
+            );
+        }
+    }
+
+    ProductRequestDTO createFakeProductRequestDTO(Long categoryId){
+        return new ProductRequestDTO(
+                "Keyboard",
+                "Mechanical Keyboard with RGB Lightning",
+                BigDecimal.valueOf(250.00),
+                "https://my-api-images.com/keyboard.jpg",
+                categoryId
+        );
+    }
+
+    ProductResponseDTO createExpectedResponse(Product product){
+        return new ProductResponseDTO(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getImgUrl(),
+                entityToResponseDTO(product.getCategory())
+        );
+    }
+
+    CategoryResponseDTO entityToResponseDTO(Category category){
+        return new CategoryResponseDTO(
+                category.getId(),
+                category.getName()
+        );
+    }
+
+    ProductRequestDTO entityToRequestDTO(Product product){
+        return new ProductRequestDTO(
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getImgUrl(),
+                product.getCategory().getId()
+        );
+    }
+
+    String getExpectedProductNotFoundMessage(Long productId){
+        return "Product not found with ID: " + productId;
     }
 }
